28f171b769a89f7ded6e73e5a5d93fc3df641234
Victor Mendes <120318287+VictorMendes0@users.noreply.github.com>
2023-06-23 04:44:31 -0300

    correções

--- js/main.js
+++ js/main.js
@@ -1,53 +1,12 @@
-$(document).ready(function(){
-    $('.product-list-promo').slick({
-    dots: true,
-    arrows: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay:true,
-    });
-
-    $('#telefone').mask('(00) 00000-0000');
-
-    $('form').validate({
-        rules: {
-            nome: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true
-            },
-            mensagem: {
-                required: true
-            }
-        },
-        messages: {
-            nome: 'Por favor, insira seu nome!',
-            telefone: 'Seu numer é muito importante',
-            email: 'Seu email é muito importante!',
-        },
-        submitHandler: function(form) {
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador) {
-            let camposIncorretos = validador.numberOfInvalids();
-            if (camposIncorretos) {
-                alert(`Existem ${camposIncorretos} campos incorretos`)
-            }
-        }
-    })
-})
-
-var buttons = document.querySelectorAll('.nav-link');
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        // LEMBRAR remove a classe "d-none" das abas correspondentes
-        var target = this.getAttribute('data-bs-target');
-        var tabPane = document.querySelector(target);
-        tabPane.classList.remove('d-none');
-    });
-}
+$(document).ready(function() {
+    // Adiciona um evento de clique aos botões da navegação
+    $('.nav-link').click(function() {
+      // Remove a classe "show" de todas as abas
+    $('.tab-pane').removeClass('show');
+      // Adiciona a classe "d-none" a todas as abas
+    $('.tab-pane').addClass('d-none');
+      // Adiciona a classe "show" à aba selecionada
+    $($(this).data('bs-target')).addClass('show');
+      // Remove a classe "d-none" da aba selecionada
+    $($(this).data('bs-target')).removeClass('d-none');
+})});
